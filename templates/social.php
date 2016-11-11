91d23ec2402ad1805baae130efe0e36d28a73caf
Pablo Garcia Egido <pgarciaegido@gmail.com>
2016-11-11 13:32:54 +0100

    Fixing few issues

--- templates/social.php
+++ templates/social.php
@@ -2,8 +2,9 @@
 	<script src="http://platform.twitter.com/widgets.js" type="text/javascript"></script>
    <a href="http://twitter.com/share" class="twitter-share-button"
       data-url="<?php the_permalink(); ?>"
-      data-via="lexcontractus"
       data-text="<?php the_title(); ?>"
       data-related="Lex Contractus"
       data-count="vertical">Tweet</a>
 </div>
+
+<!-- data-via="lexcontractus" -->
