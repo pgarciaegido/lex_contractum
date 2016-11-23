3304989ce78d70da8a96836a467c0dc8e36fc230
Pablo Garcia Egido <pgarciaegido@gmail.com>
2016-11-23 13:37:41 +0100

    fixing minor issiue in footer

--- footer.php
+++ footer.php
@@ -12,7 +12,7 @@
 	</div>
 		<nav class="Footer-menu">
 			<ul class="Footer-menu-list">
-				<a href="lexcontractus.es"><li class="Footer-menu-item">HOME</li></a>
+				<a href="http://lexcontractus.es"><li class="Footer-menu-item">HOME</li></a>
 				<a href="http://lexcontractus.es/category/mercantil/"><li class="Footer-menu-item">MERCANTIL</li></a>
 				<a href="http://lexcontractus.es/category/fiscal-internacional/"><li class="Footer-menu-item">FISCAL INTERNACIONAL</li></a>
 			</ul>
@@ -21,10 +21,14 @@
 				<a href="http://lexcontractus.es/quienes-somos/"><li class="Footer-menu-item">¿QUIENES SOMOS?</li></a>
 				<a href="http://lexcontractus.es/contacta/"><li class="Footer-menu-item">CONTACTO</li></a>
 			</ul>
-			<a href="https://www.linkedin.com/in/pablo-garc%C3%ADa-egido-42115359?trk=nav_responsive_tab_profile_pic"><div class="Footer-autor">
-				<p class="Footer-autor-texto">DESIGN &</br> DEVELOPMENT</p>
-				<img src="<?php bloginfo('template_url'); ?>/images/pegido.svg" alt="Pablo Egido" class="Footer-autor-logo"></a>
+			<!-- DESIGN AND DEVELOPMENT -->
+			<div class="Footer-autor">
+				<a href="https://www.linkedin.com/in/pablo-garc%C3%ADa-egido-42115359?trk=nav_responsive_tab_profile_pic">
+					<p class="Footer-autor-texto">DESIGN &</br> DEVELOPMENT</p>
+					<img src="<?php bloginfo('template_url'); ?>/images/pegido.svg" alt="Pablo Egido" class="Footer-autor-logo">
+				</a>
 			</div>
+			<!-- DESIGN AND DEVELOPMENT -->
 		</nav>
 		<p class="Footer-copymob">Lex Contractus | 2016</p>
 	</footer>
