00e84a004beb2f0c32b77356c86ab6c8763febd2
Pablo Garcia Egido <pgarciaegido@gmail.com>
2016-11-28 12:46:13 +0100

    Implementing gulp

---
+++ gulpfile.js
@@ -0,0 +1,55 @@
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var sass = require ('gulp-sass');
+// var livereload = require('gulp-livereload');
+var imagemin = require('gulp-imagemin');
+var autoprefixer = require('gulp-autoprefixer');
+var rename = require('gulp-rename');
+
+function errorLog(error){
+	console.error.bind(error);
+	this.emit('end');
+}
+
+// Scripts Task
+// Uglifies
+gulp.task('scripts', function(){
+	gulp.src('./index.js')
+			.pipe(uglify())
+			.on('error', errorLog)
+			.pipe(gulp.dest('build/js'));
+});
+
+//Styles Task
+gulp.task('styles', function(){
+	gulp.src('styles/index.scss')
+			.pipe(sass({
+				outputStyle: 'compressed'
+			}))
+			.on('error', errorLog)
+			.pipe(autoprefixer('> 5%'))
+			.pipe(rename('style.css'))
+			.pipe(gulp.dest('./'))
+			// .pipe(livereload());
+});
+
+//Image Task
+//Compress
+gulp.task('image', function(){
+	gulp.src('images/*')
+			.pipe(imagemin())
+			.pipe(gulp.dest('./images'));
+});
+
+
+//Watch task (built-in method)
+//Watches JS
+gulp.task('watch', function(){
+
+	// var server = livereload();
+
+	gulp.watch('./index.js',  ['scripts']);
+	gulp.watch('styles/index.scss', ['styles']);
+});
+
+gulp.task('default', ['scripts', 'styles', 'watch']);
